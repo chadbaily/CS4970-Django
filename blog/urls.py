f5fc0a596e7bd2037b3fffcea35edd4526eaaf26
Chad Baily <chadbaily27@gmail.com>
2018-09-13 15:54:50 -0400

    spelling mistake fix

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import post_model_lsit_view
+from .views import post_model_list_view
 
 urlpatterns = [
     url(r'^$', post_model_list_view, name='list'),
