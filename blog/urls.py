7cc5c6f800ebc589657dbe804bec6080ed281f28
Chad Baily <chadbaily27@gmail.com>
2018-09-13 15:52:23 -0400

    fix import

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,6 @@
-from django.urls import path
+from django.conf.urls import url
 
 from .views import post_model_lsit_view
-
-app_name = 'blog'
-
 
 urlpatterns = [
     url(r'^$', post_model_list_view, name='list'),
