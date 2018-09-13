6a86d0a4a676f5568ba9e48f678ddb378efcc060
Sarah Smith <sarah@d-172-25-99-62.dhcp.virginia.edu>
2018-09-13 16:06:45 -0400

    new form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import PostModelForm
 from .models import PostModel
 
 # Create your views here.
@@ -9,3 +9,17 @@ def post_model_list_view(request):
     qs = PostModel.objects.all()
     print(qs)
     return HttpResponse("some data")
+
+def post_model_create_view(request):
+    template = "blog/create-view.html"
+    form = PostModelForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        context = {
+            "form": PostModelForm()
+        }
+    return render(request, template, context)
