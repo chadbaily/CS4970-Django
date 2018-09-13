0ff0925aca08e81a616aa1094e9ebf7ae23be020
Sarah Smith <sarah@d-172-25-99-62.dhcp.virginia.edu>
2018-09-13 16:39:13 -0400

    fix detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from .views import (post_model_list_view, post_model_create_view,)
+from .views import (post_model_list_view, post_model_create_view, post_model_detail_view)
 
 urlpatterns = [
     url(r'^$', post_model_list_view, name='list'),
     url(r'^create/$', post_model_create_view, name='create'),
+    url(r'^blog/',include(('blog.urls','blog'),namespace='blog')),
+    url(r'^(?P<id>\d+)/$', post_model_detail_view, name='detail'),
 ]
