7cc8de2fa168f1a948ac66f3aa6d1655834b896c
Sarah Smith <sarah@d-172-25-99-62.dhcp.virginia.edu>
2018-09-13 16:52:33 -0400

    defined update view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import (post_model_list_view, post_model_create_view, post_model_detail_view)
+from .views import (post_model_list_view, post_model_create_view, post_model_detail_view, post_model_update_view,)
 
 urlpatterns = [
     url(r'^$', post_model_list_view, name='list'),
